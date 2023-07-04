6eabaf38ea6cd8be307b7a5364341eea28c028ae
yonatanz <yonatanz@earnix.com>
2023-07-04 12:19:06 +0300

    the solution

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,19 +1,20 @@
-import { Component } from "react";
+import { ChangeEvent, Component } from "react";
 import { TopBar } from "./TopBar";
 import { GoogleMap } from "./GMaps";
 // const log = (...args) => console.log.apply(null, ["App -->", ...args]);
 const log = (...args: any[]) => console.log("App -->", ...args);
 
-
 interface State {
   lat: number;
   lng: number;
+  zoom: number;
 }
 
 export class App extends Component<object, State> {
   state = {
     lat: -34.397,
     lng: 150.644,
+    zoom: 8,
   };
 
   reposition(city: string) {
@@ -21,21 +22,51 @@ export class App extends Component<object, State> {
       case "tel aviv":
         this.setState({ lat: 32.0042938, lng: 34.7615399 });
         break;
+      case "London":
+        this.setState({ lat: 51.5287393, lng: -0.2667477 });
+        break;
+      case "Paris":
+        this.setState({ lat: 48.8589383, lng: 2.264463 });
+        break;
+      case "Me":
+        const pos = navigator.geolocation.getCurrentPosition((val) => {
+          this.setState({
+            lat: val.coords.latitude,
+            lng: val.coords.longitude,
+          });
+        });
+        break;
       default:
         alert("wrong city");
     }
   }
 
+  zoom = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ zoom: parseInt(event.target.value) });
+  };
+
   render() {
     log(this.state);
     return (
       <div className="app">
-        <TopBar><h1>Google Maps Example in React</h1></TopBar>
+        <TopBar>
+          <h1>Google Maps Example in React</h1>
+        </TopBar>
         <div className="hbox mb20">
           <button onClick={() => this.reposition("tel aviv")}>Tel Aviv</button>
-          <input type="number" min="8" max="16" placeholder="8" />
+          <button onClick={() => this.reposition("London")}>London</button>
+          <button onClick={() => this.reposition("Paris")}>Paris</button>
+          <button onClick={() => this.reposition("Me")}>Locate me</button>
+
+          <input
+            type="number"
+            min="8"
+            max="16"
+            placeholder="8"
+            onChange={this.zoom}
+          />
         </div>
-        <GoogleMap lat={this.state.lat} lng={this.state.lng} />
+        <GoogleMap {...this.state} />
       </div>
     );
   }
--- src/view/GMaps.tsx
+++ src/view/GMaps.tsx
@@ -3,13 +3,14 @@ import React, { Component } from "react";
 const log = (...args: any[]) => console.log("GoogleMap -->", ...args);
 
 interface Props {
-    lat: number;
-    lng: number;
+  lat: number;
+  lng: number;
+  zoom: number;
 }
 
 export class GoogleMap extends Component<Props> {
-
   mapRef = React.createRef<HTMLDivElement>();
+  inputRef = React.createRef<HTMLInputElement>();
   theMap: google.maps.Map | null = null;
 
   shouldComponentUpdate(nextProps: Props) {
@@ -19,7 +20,11 @@ export class GoogleMap extends Component<Props> {
     // log("nextState:", nextState);
     // log("nextProps:", nextProps);
     // log("<<<< shouldComponentUpdate");
-    (this.theMap as google.maps.Map).setCenter({ lat: nextProps.lat, lng: nextProps.lng });
+    (this.theMap as google.maps.Map).setCenter({
+      lat: nextProps.lat,
+      lng: nextProps.lng,
+    });
+    (this.theMap as google.maps.Map).setZoom(this.props.zoom);
 
     return false;
   }
@@ -28,12 +33,38 @@ export class GoogleMap extends Component<Props> {
     // log(this.mapRef);
     this.theMap = new google.maps.Map(this.mapRef.current as HTMLDivElement, {
       center: { lat: this.props.lat, lng: this.props.lng },
-      zoom: 8
+      zoom: 8,
     });
   }
 
-  render() {
-    return <div ref={this.mapRef} className="map-box" />;
-  }
+  addMarker = () => {
+    const currCenter = this.theMap?.getCenter();
+    const title = this.inputRef.current?.value;
+    const marker = new google.maps.Marker({
+      position: currCenter,
+      map: this.theMap,
+      title: title,
+      clickable: true,
+    });
 
+    var infowindow = new google.maps.InfoWindow({
+      content: title,
+    });
+
+    const theMap = this.theMap as google.maps.Map;
+
+    google.maps.event.addListener(marker, "click", function () {
+      infowindow.open(theMap, marker);
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <input type="text" placeholder="Marker title" ref={this.inputRef} />
+        <button onClick={this.addMarker}>Add marker</button>
+        <div ref={this.mapRef} className="map-box" />
+      </>
+    );
+  }
 }
